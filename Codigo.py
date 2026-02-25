c2d028f5e060bb51602661e1c052ef4eb75e8f9c
Joao-Vitor50 <joaovsds50@gmail.com>
2026-02-25 18:39:41 -0300

    Updated Codigo.py
    Um update deste código no segundo dia

--- Codigo.py
+++ Codigo.py
@@ -1,23 +1,49 @@
-#Perguntas para escolher os numeros e o sinal
- Primeiro_valor = int(input("Digite o primeiro valor: "))
- Segundo_valor = int(input("Digite o primeiro valor: "))
- sinal = input("Digite o sinal correspondente( soma, subtração, mutiplicação ou divisão): ")
- if Primeiro_valor or Segundo_valor == "" or sinal != "soma" or "mutiplicação" or "subtração" or "divisão":
- print("valor ou sinal imcompativél, Porfavor, tente denovo")
+#Fazendo o famoso Projeto Frankeistain
+#Calculadora basica
 
-#Condições
-if sinal == "soma":
- resultado = Primeiro_valor + Segundo_valor
- print(f"o resultado de {Primeiro_valor} somado {Segundo_valor} é {resultado}")
- 
-elif sinal == "subtração":
- resultado = Primeiro_valor - Segundo_valor
- print(f"o resultado de {Primeiro_valor} subtraido{Segundo_valor} é {int(resultado)}")
-elif sinal == "mutiplicação":
- resultado = Primeiro_valor * Segundo_valor
- print(f"o resultado de {Primeiro_valor} vezes {Segundo_valor} é {int(resultado)}")
-elif sinal == "divisão":
- resultado = Primeiro_valor / Segundo_valor 
- print(f"o resultado de {Primeiro_valor} dividido por {Segundo_valor} é {int(resultado)}")
-elif sinal != "soma" or "subtração" or "mutiplicação" or "divisão":
- print("valor incompativél")
+#Perguntas para escolher os numeros e o sinal.
+while True:
+ while True:
+  Primeiro_valor = input("Digite o primeiro valor: ")
+  Segundo_valor = input("Digite o primeiro valor: ")
+  sinal = input("Digite o sinal correspondente (soma, subtração, mutiplicação ou divisão): ")
+
+  try: #Verificar se os valores estão corretamente digitados.(try e except foram comandos novos que aprendi no dia 2)
+   Primeiro_valor = int(Primeiro_valor)
+   Segundo_valor = int(Segundo_valor)
+   sinal == ["soma", "subtração", "mutiplicação", "divisão"]
+  except: #caso um desses valores estejam errados, o comando abaixo irar rodar.
+   print("valor ou sinal imcompativél, Porfavor, tente denovo")
+   continue
+  else:
+   break
+
+ #Condições
+ if sinal == "soma":
+  resultado = Primeiro_valor + Segundo_valor
+  print(f"o resultado de {Primeiro_valor} somado {Segundo_valor} é {resultado}") 
+ elif sinal == "subtração":
+  resultado = Primeiro_valor - Segundo_valor
+  print(f"o resultado de {Primeiro_valor} subtraido{Segundo_valor} é {int(resultado)}")
+ elif sinal == "mutiplicação":
+  resultado = Primeiro_valor * Segundo_valor
+  print(f"o resultado de {Primeiro_valor} vezes {Segundo_valor} é {int(resultado)}")
+ elif sinal == "divisão":
+  resultado = Primeiro_valor / Segundo_valor 
+  print(f"o resultado de {Primeiro_valor} dividido por {Segundo_valor} é {int(resultado)}")
+
+ while True:#isso ainda não funciona por algum motivo
+  SAIDA = input("Deseja fazer outro calculo?(S/N): ")#uma opção de saida que apareçera para o usuario depois de ter feito o calculo 
+  if SAIDA == "S":
+   continue
+  elif SAIDA == "N":
+   print("Saindo do Aplicativo...")
+   print("Aplicativo finalizado")
+   break
+  else:
+   print("Opção invalida")
+   continue
+
+
+#LISTA DE IDEIAS
+#Fazer uma simulação de sistemas operacionais, ou sejá, um programa que me permite navegar e acessar aplicativos dentro dele mesmo, como essa calculadora.
